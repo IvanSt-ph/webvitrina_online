4dc820551a4793211defb9585cc0b167e0884df3
IvanSt <1337friend.ivan@gmail.com>
2025-12-02 19:50:23 +0200

    1.3.2.2 Добавил кнопку плдвердить эмаил у продоацва

--- resources/views/seller/partials/main.blade.php
+++ resources/views/seller/partials/main.blade.php
@@ -103,4 +103,23 @@
 
     </div>
   </form>
+
+  @if (!Auth::user()->hasVerifiedEmail())
+  <form method="POST" action="{{ route('verification.send') }}" class="mt-3">
+    @csrf
+    <button type="submit"
+            class="px-4 py-2 bg-orange-500 hover:bg-orange-600 text-white text-xs 
+                   rounded-lg shadow-sm flex items-center gap-2 transition">
+      <i class="ri-mail-send-line"></i>
+      Отправить письмо подтверждения
+    </button>
+  </form>
+
+  @if (session('status') === 'verification-link-sent')
+    <p class="text-xs text-green-600 mt-2 flex items-center gap-1">
+        <i class="ri-check-double-line"></i>
+        Письмо отправлено! Проверь свою почту.
+    </p>
+@endif
+@endif
 </section>
