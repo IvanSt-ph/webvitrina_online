e32dd3975597e1eebc3d6067166dbbd117a49390
terkinwasiya999 <terkinwasiya999@gmail.com>
2026-04-18 21:20:36 +0300

    ку v3

--- routes/web.php
+++ routes/web.php
@@ -364,9 +364,6 @@ Route::middleware('role:buyer')->group(function () {
 Route::get('/seller/{user}', [SellerController::class, 'show'])->name('seller.show');
 
 
-Route::get('/cart/count', [CartController::class, 'getCount'])->name('cart.count');
-
-
 /*
 |--------------------------------------------------------------------------
 | 🌐 GOOGLE LOGIN
